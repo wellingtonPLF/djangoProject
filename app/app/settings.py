eb36bd3a16c0d9c15ef6c1c19d4452394b9e6fa5
WellingtonPLF <42122392+wellingtonPLF@users.noreply.github.com>
2022-10-09 08:38:46 -0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+# When using .env make sure to set default as [].
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='0.0.0.0', cast=Csv())
 
 # Application definition
 
